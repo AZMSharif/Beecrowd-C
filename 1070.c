eff9a85ddfafd7d246c220fd24ea46d2453c75c0
AZMSharif <110534962+AZMSharif@users.noreply.github.com>
2022-08-15 21:30:43 +0600

    Create 1070.c

---
+++ 1070.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+    int X,i;
+    scanf("%d", &X);
+    if(X%2==0)
+    {
+        for(i=X; i<=(X+12); i++)
+        {
+            if(i%2!=0)
+            {
+                printf("%d\n", i);
+            }
+        }
+    }
+        else
+        {
+            for(i=X; i<=(X+10); i++)
+            {
+                if(i%2!=0)
+                {
+                    printf("%d\n", i);
+                }
+            }
+        }
+        return 0;
+    }
